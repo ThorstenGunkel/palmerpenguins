783f2852c61cd4955fc77e102e77b882ab0718dd
ThorstenGunkel <Thorsten.Gunkel@posteo.de>
2020-08-27 20:07:00 +0200

    added marker

--- 2_penguin_shiny.R
+++ 2_penguin_shiny.R
@@ -2,7 +2,6 @@ library(shiny)
 library(plotly)
 
 ui <- fluidPage(
-#  selectInput("choice", "Choose", choices = names(iris), selected = NULL),
   selectInput("choice", "Choose", choices = names(penguins_clean), selected = NULL),
   plotlyOutput("graph")
 )
@@ -10,11 +9,6 @@ ui <- fluidPage(
 server <- function(input, output, session){
   
   output$graph <- renderPlotly({
-#    plot_ly(penguins_clean, x = ~get(input$choice), y = ~bill_length_mm, type = 'scatter', mode = 'markers', color = ~species)
-    #this works: 2D interactive image
-    
-#    plot_ly(penguins_clean, x = ~get(input$choice), y = ~bill_length_mm, z = ~bill_depth_mm, color = ~species, size = 1)
-# geht,jetzt aber mit annotation
 
     plot_ly(penguins_clean, 
             x = ~get(input$choice), 
@@ -22,19 +16,13 @@ server <- function(input, output, session){
             z = ~bill_depth_mm, 
             color = ~species, 
             size = 1, 
-#            type="scatter3d") %>%
-            type="scatter3d") %>%
-            add_trace(mode = 'lines') %>%
+            type="scatter3d",
+            mode = "markers") %>%
 
-      
-      
-              layout(scene = list(xaxis = list(title = 'bill length (mm)'),
-                                      yaxis = list(title = 'bill depth (mm)'),
-                                      zaxis = list(title = 'flipper lenth (mm)')),
-                         title = "Penguins!")
-    
-    
-#programm meckert wg plot_ly: daher automatisch scatter3d    
+            layout(scene = list(xaxis = list(title = 'bill length (mm)'),
+                                    yaxis = list(title = 'bill depth (mm)'),
+                                    zaxis = list(title = 'flipper lenth (mm)')),
+                           title = "Penguins!")
     
       })
 }
